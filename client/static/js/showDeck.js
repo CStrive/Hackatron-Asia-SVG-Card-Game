006e7d3e31fc3db2e8ab1d5b5cd6a51de4fdd00d
sudarsangp <sudarsangp@gmail.com>
2014-12-06 14:27:03 +0800

    show only latest card drawn

--- client/static/js/showDeck.js
+++ client/static/js/showDeck.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var cardDeck = $("#cardDeck").playingCards();
 	
 	var hand = [];
+	var drawnCards = [];
 	var showError = function(msg){
         $('#error').html(msg).show();
         setTimeout(function(){
@@ -9,11 +10,9 @@ $(document).ready(function() {
         },3000);
     }
     var showHand = function(){
-        var el = $('#yourHand')
+        var el = $('#yourHand');
         el.html('');
-        for(var i=0;i<hand.length;i++){
-            el.append(hand[i].getHTML());
-        }
+        el.append(hand[hand.length-1].getHTML());
     }
 	var doDrawCard = function(){
         var c = cardDeck.draw();
