fef3a44f59f3bf5fa1b597f008c716e0c81593c4
Raghav Ramesh <raghavramesh94@gmail.com>
2014-12-06 14:44:43 +0800

    feat: closeFace(); remove document ready

--- client/static/js/showDeck.js
+++ client/static/js/showDeck.js
@@ -1,29 +1,37 @@
-$(document).ready(function() {
-	var cardDeck = $("#cardDeck").playingCards();
-	
-	var hand = [];
-	var showError = function(msg){
-        $('#error').html(msg).show();
-        setTimeout(function(){
-            $('#error').fadeOut('slow');
-        },3000);
+
+var cardDeck = $("#cardDeck").playingCards();
+
+var hand = [];
+var showError = function(msg){
+    $('#error').html(msg).show();
+    setTimeout(function(){
+        $('#error').fadeOut('slow');
+    },3000);
+}
+var showHand = function(){
+    var el = $('#yourHand')
+    el.html('');
+    for(var i=0;i<hand.length;i++){
+        el.append(hand[i].getHTML());
     }
-    var showHand = function(){
-        var el = $('#yourHand')
-        el.html('');
-        for(var i=0;i<hand.length;i++){
-            el.append(hand[i].getHTML());
-        }
+}
+function closeCard() {
+    var closeCardImage = document.createElement("div");
+    closeCardImage.style.width="76px";
+    closeCardImage.style.height="100px";
+    closeCardImage.style.backgroundImage="url('static/img/facedowncard.jpg')";
+    var el = $('#yourHand');
+    el.html('');
+    el.append(closeCardImage);
+}
+var doDrawCard = function(){
+    var c = cardDeck.draw();
+    if(!c){
+        showError('no more cards');
+        return;
     }
-	var doDrawCard = function(){
-        var c = cardDeck.draw();
-        if(!c){
-            showError('no more cards');
-            return;
-        }
-        hand[hand.length] = c;
-        //cardDeck.spread();
-        showHand();
-    }
-    $('#draw').click(doDrawCard);
-});
+    hand[hand.length] = c;
+    //cardDeck.spread();
+    showHand();
+}
+$('#draw').click(doDrawCard);
