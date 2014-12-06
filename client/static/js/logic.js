1a4f9cf48f8ae7a966d0a1915e2f7cdc1ba2baa3
vigneshRajakumar <vigneshr.93@gmail.com>
2014-12-06 20:40:44 +0800

    feat:skeleton for server comm

--- client/static/js/logic.js
+++ client/static/js/logic.js
@@ -9,17 +9,31 @@ startGame();
 
 // Set deck clickable for current player 
 $('#draw').attr("disabled", false);
-drawnCard = doDrawCard()['rank'];
+firstDrawnCard = doDrawCard()['rank'];
 
 // Show card, with possible actions
 
 // Implement actions
+var clientAction = function(action) {
+
+	//1.Perform action
+	if(action['name'] == 'swap') {
+		console.log(typeof(action['id']));
+		swapCard(action['id']);
+	}
+	//2.Inform Server
+}
+
+var serverOrder = function() {
+	//1.Perfrom requested action
+}
 
 // 1. Replace player card with draw card, add card to pool
 
 // 2. Add card to pool
 
 // 3. Special card
+
 if(drawnCard == 'K') {
 	showP2Hand();
 } else if(drawnCard == 'Q') {
@@ -32,42 +46,9 @@ if(drawnCard == 'K') {
 
 // Jump to set player turn state
 
-// Decide winner by comparing total of hand values
-var p1HandValue = computeHandValue(player1Hand);
-var p2HandValue = computeHandValue(player2Hand);
-
-if (p1HandValue < p2HandValue) {
-	console.log("P1 wins!");
-} else if (p1HandValue > p2HandValue) {
-	console.log("P2 wins!");
-} else {
-	console.log("Tie!");
-}
-
-function computeHandValue(hand) {
-	var value = 0;
-	for (var i = 0; i < 3; i++) {
-		if (hand[i]['rank'] == 'A') {
-			value = value + 1;
-		} else if (hand[i]['rank'] == 'J' || hand[i]['rank'] == 'Q' || hand[i]['rank'] == 'K') {
-			value = value + 10;
-		} else {
-			value = value + parseInt(hand[i]['rank']);	
-		}	
-	}
-	return value;
-}
-
 function closeFace(id) {
 	// Replace with a close card image
 	closeCard(id);
 }
 
-function popCardFromDeck(rank, suit) {
-	var tempCard = playingCards.card(rank, suit);
-	for (var i = 0; i<cardDeck['cards'].length; i++) {
-		if (cardDeck['cards'][i] != undefined && cardDeck['cards'][i]['rank'] == tempCard['rank'] && cardDeck['cards'][i]['suit'] == tempCard['suit']) {
-			delete cardDeck['cards'][i];
-		}
-	}
-}
+
